00e47176687a471ffae6a80d10b7072792300668
Thorben Menne <mennthor@aol.com>
2016-09-30 13:20:29 +0200

    Added ipykernel, nbextensions, replaced comments with echoes.

--- osx/python.sh
+++ osx/python.sh
@@ -2,16 +2,33 @@
 ## without polluting the system install location.
 
 
-# First install pip in the user folder ${HOME}/Library/Pythom/2.7
-echo "  Installing pip ..."
+echo "  Install pip in the user folder ~/Library/Python/2.7 ..."
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py --user
 rm get-pip.py
 
-# Tell python to prefer user packages (check later with `$ python -m site`)
-echo "  Create 'local.pth' in ~/Library/Python/..."
+echo "  Create 'local.pth' in python user folder to prefer the user packages"
+echo "  over the systems ones. Check paths later with `$ python -m site` ..."
 echo "import sys; sys.path.insert(1,'${HOME}/Library/Python/2.7/lib/python/site-packages')" >> ${HOME}/Library/Python/2.7/lib/python/site-packages/local.pth
 
-# Install all in pip_requirements file to user folder (similar to Brewfile)
-echo "  Upgrade and install packages in pip_requirements.txt ..."
-${HOME}/Library/Python/2.7/bin/pip2 install --upgrade --user -r "./osx/pip_requirements.txt"
+echo "  Upgrade and install packages from homebrew/pip_requirements.txt ..."
+pip install --upgrade --user -r "./homebrew/pip_requirements.txt"
+
+echo "  Install ipython 2 kernel for ipython2 notebook ..."
+python -m pip install ipykernel --user
+python -m ipykernel install --user --display-name "Python 2 (OSX)"
+# http://ipython.readthedocs.io/en/stable/install/kernel_install.html
+
+echo "  Install notebook extensions ..."
+pip install --user https://github.com/ipython-contrib/jupyter_contrib_nbextensions/tarball/master
+jupyter-contrib-nbextension install --user
+# https://github.com/ipython-contrib/jupyter_contrib_nbextensions
+
+# Enable extensions in notebook section
+jupyter-nbextension enable collapsible_headings/main
+jupyter-nbextension enable hide_input_all/main
+jupyter-nbextension enable ruler/main
+jupyter-nbextension enable codefolding/main
+jupyter-nbextension enable init_cell/main
+# Enable extensions in tree section
+jupyter-nbextension enable nbextensions_configurator/tree_tab/main
