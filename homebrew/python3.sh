ef4c0cdfc7c220410149cd44af095305b36e748a
tmenne <thorben.menne@tu-dortmund.de>
2016-10-06 08:05:33 +0200

    Linking jupyter in py3 user folder to jupyter3 consistenly with pip3 and ipython3 commands.

--- homebrew/python3.sh
+++ homebrew/python3.sh
@@ -1,4 +1,5 @@
 # Script to setup, update and upgrade the homebrew python3
+# User folder is ~/Library/Python/3.5
 
 echo "  Upgrade and install packages from homebrew/pip_requirements.txt ..."
 pip3 install --upgrade --user -r "./homebrew/pip_requirements.txt"
@@ -6,3 +7,10 @@ pip3 install --upgrade --user -r "./homebrew/pip_requirements.txt"
 echo "  Install ipython 3 kernel for ipython2 notebook ..."
 python3 -m pip install ipykernel --user
 python3 -m ipykernel install --user --display-name "Python 3 (Homebrew)"
+
+echo "Link 'jupyter-*' binaries in py3 user folder to 'jupyter3-*'."
+cd ${HOME}/Library/Python/3.5/bin
+for j in ./jupyter*; do
+	ln -s $j "${j:0:9}3${j:9:100}";
+done
+cd -
