7944a7cdd387ee09449b844a1e0110195a99519d
tmenne <thorben.menne@tu-dortmund.de>
2016-10-06 11:53:35 +0200

    Added symlink to py3 user folder, so that python is py3 if first in path.

--- homebrew/python3.sh
+++ homebrew/python3.sh
@@ -8,9 +8,13 @@ echo "  Install ipython 3 kernel for ipython2 notebook ..."
 python3 -m pip install ipykernel --user
 python3 -m ipykernel install --user --display-name "Python 3 (Homebrew)"
 
-echo "Link 'jupyter-*' binaries in py3 user folder to 'jupyter3-*'."
+echo "  Link 'jupyter-*' binaries in py3 user folder to 'jupyter3-*'."
 cd ${HOME}/Library/Python/3.5/bin
 for j in ./jupyter*; do
 	ln -s $j "${j:0:9}3${j:9:100}";
 done
+
+echo "  Make `python` link in py3 user folder for make scripts using `python`."
+# When py3 is the first path object, then python points to py3
+ln -s /usr/local/bin/python3 ./python
 cd -
