16f8c1571f8e3df4ffac517fcb75382ac0dc14a3
Thorben Menne <thorben.menne@tu-dortmund.de>
2017-03-22 10:50:34 +0100

    Autodetection of python3 version in brew py3 setup script

--- homebrew/python3.sh
+++ homebrew/python3.sh
@@ -1,15 +1,16 @@
 # Script to setup, update and upgrade the homebrew python3
-# User folder is ~/Library/Python/3.5
+# User folder is ~/Library/Python/3.* (autodetected with PY3VERSION)
 
 echo "  Upgrade and install packages from homebrew/pip_requirements.txt ..."
 pip3 install --upgrade --user -r "./homebrew/pip_requirements.txt"
 
-echo "  Install ipython 3 kernel for ipython2 notebook ..."
+echo "  Install ipython 3 kernel for ipython notebook ..."
 python3 -m pip install ipykernel --user
 python3 -m ipykernel install --user --display-name "Python 3 (Homebrew)"
 
 echo "  Link 'jupyter-*' binaries in py3 user folder to 'jupyter3-*'."
-cd ${HOME}/Library/Python/3.5/bin
+PY3VERSION=$(python3 -c "import sys; print('{}.{}'.format(*sys.version_info[:2]))")
+cd ${HOME}/Library/Python/"$PY3VERSION"/bin
 for j in ./jupyter*; do
 	ln -s $j "${j:0:9}3${j:9:100}";
 done
