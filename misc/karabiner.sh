6e4b69b11a2874e7f97897cc9377fceb7f9faa1b
Thorben Menne <thorben.menne@tu-dortmund.de>
2017-06-28 09:16:17 -0400

    Karabiner has a nice cli mode to save settings in a shell script. Uing that

---
+++ misc/karabiner.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# See here: https://pqrs.org/osx/karabiner/document.html.en#export
+# Save settings with:
+# >>> karabiner export >> karabiner.sh
+# Load settings with:
+# >>> karabiner be_careful_to_use__clear_all_values_by_name PROFILE_NAME
+# >>> bash karabiner.sh
+
+cli=/usr/local/bin/karabiner
+
+$cli set remap.controlL2controlL_escape 1
+/bin/echo -n .
+$cli set remap.controlL2fn 1
+/bin/echo -n .
+$cli set remap.escape2capslock 1
+/bin/echo -n .
+$cli set remap.fn_consumer_to_fkeys_f1 1
+/bin/echo -n .
+$cli set remap.fn_consumer_to_fkeys_f10 1
+/bin/echo -n .
+$cli set remap.fn_consumer_to_fkeys_f3 1
+/bin/echo -n .
+$cli set remap.fn_consumer_to_fkeys_f5 1
+/bin/echo -n .
+$cli set remap.fn_consumer_to_fkeys_f7 1
+/bin/echo -n .
+$cli set remap.fn2controlL 1
+/bin/echo -n .
+$cli set repeat.initial_wait 150
+/bin/echo -n .
+$cli set repeat.wait 10
+/bin/echo -n .
+/bin/echo
