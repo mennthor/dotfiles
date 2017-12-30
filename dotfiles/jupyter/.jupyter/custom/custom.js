f955a971ca2db62f366316f60b94d8fac3ba86fe
Thorben Menne <thorben.menne@tu-dortmund.de>
2017-12-30 21:16:32 +0100

    jupyter :: Added custom.js to include sublime keybindings for notebook cells. From http://blog.rtwilson.com/ :+1:

---
+++ dotfiles/jupyter/.jupyter/custom/custom.js
@@ -0,0 +1,14 @@
+// Use line shift and Cmd-D multicursors in notebooks
+// From: http://blog.rtwilson.com/how-to-get-sublime-text-style-editing-in-the-ipythonjupyter-notebook/
+require(["codemirror/keymap/sublime", "notebook/js/cell", "base/js/namespace"],
+    function(sublime_keymap, cell, IPython) {
+        // setTimeout(function(){ // uncomment line to fake race-condition
+        cell.Cell.options_default.cm_config.keyMap = 'sublime';
+        var cells = IPython.notebook.get_cells();
+        for(var cl=0; cl< cells.length ; cl++){
+            cells[cl].code_mirror.setOption('keyMap', 'sublime');
+        }
+
+        // }, 1000)// uncomment  line to fake race condition
+    }
+);
