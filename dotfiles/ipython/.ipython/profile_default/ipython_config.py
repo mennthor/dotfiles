0f80b717a6f4132848a3959294c6411aa78970e7
Thorben Menne <mennthor@aol.com>
2018-08-24 12:28:42 +0200

    Removed auto loading of packages on ipython startup, interferes with mpl backend choices

--- dotfiles/ipython/.ipython/profile_default/ipython_config.py
+++ dotfiles/ipython/.ipython/profile_default/ipython_config.py
@@ -5,12 +5,12 @@
 #------------------------------------------------------------------------------
 
 ## lines of code to run at IPython startup.
-c.InteractiveShellApp.exec_lines = [
-    "from __future__ import print_function, division",
-    "import numpy as np",
-    "import matplotlib.pyplot as plt",
-    "import scipy.stats as scs"
-]
+# c.InteractiveShellApp.exec_lines = [
+#     "from __future__ import print_function, division",
+#     "import numpy as np",
+#     "import matplotlib.pyplot as plt",
+#     "import scipy.stats as scs"
+# ]
 
 # http://koo.fi/blog/2016/08/04/enable-vi-editing-mode-in-ipython-5/
 c.TerminalInteractiveShell.editing_mode = 'emacs'
